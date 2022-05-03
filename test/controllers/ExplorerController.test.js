003a81526180148164d27ebb76038b0abb711d56
Uriel Dávila <uri.davi.14@gmail.com>
2022-05-03 16:25:07 -0500

    15. Agregando pruebas a ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,15 @@
+const ExplorerController = require('./../../lib/controllers/ExplorerController');
+describe("Pruebas para el Explorer controller",()=>{
+    test("Prueba para obtener explorer por mision", ()=>{
+        const explorers =ExplorerController.getExplorersByMission('java');
+        expect(explorers.length).toBe(5)
+    })
+    test("Prueba para listar los nombres de usuario", ()=>{
+        const userExplorers = ExplorerController.getExplorersUsernamesByMission('java');
+        expect(userExplorers).toStrictEqual(['ajolonauta6', 'ajolonauta7','ajolonauta8','ajolonauta9','ajolonauta10'])
+    })
+    test("Obtener cantidad de usuarios por misión", ()=>{
+        const userExplorersLength = ExplorerController.getExplorersAmonutByMission('node');
+        expect(userExplorersLength).toBe(10);
+    })
+})
